00c6edd27a8eb6a1ad51330da6fe01c4590985ba
HieuNguyen1905 <120067573+HieuNguyen1905@users.noreply.github.com>
2023-01-27 15:20:52 -0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # module10-team-profile-generator
+
+## Description
+
+- This application will generate HTML page base on the user iput.
+![Html Page](./screenshoot.png)
+
+## Installation
+
+- This application does not have a deloyed link, please install node.js and run the comman node generateHtml.js
+
+## Feature 
+
+- This application will ask for employee information and roll and user can add as many employee as they need before quiting the application to generate HTML page.
--- generateHtml.js
+++ generateHtml.js
@@ -4,63 +4,19 @@ const fs = require("fs");
 const Employee = require("./lib/Employee");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const generateTeam = require("./lib/generateTeam")
 const Manager = require("./lib/Manager");
+const path = require("path");
+const team = [];
 
+promptEmployee();
 // TODO:Prompt manager
-inquirer.prompt([
-    {
-        type: 'input',
-        name:'name',
-        message: 'Enter the team manager name:',
-    },
-    {
-        type: 'input',
-        name:'id',
-        message: 'Enter the employee ID:',
-    },
-    {
-        type: 'input',
-        name:'email',
-        message: 'Enter the employee email:',
-    },
-    {
-        type: 'input',
-        name:'number',
-        message: 'Enter the office number:',
-    },
-    {
-        type: 'list',
-        name: 'question',
-        //TODO: add list of options
-        choices: ['Add an Engineer', 'Add an Intern', 'Finish building my team',]
-    }
-]).then((answers) =>{
-    switch(answers.question){
-        case 'Add an Engineer':
-            console.log('add engineer!');
-            addEngineer();
-            break;
-
-        case 'Add an Intern':
-            console.log('add intern!');
-            addIntern();
-            break;
-
-        default:
-            console.log('Your HTML has been created');
-            break;
-    }
-    const manager = new Manager(answers.name, answers.id, answers.email, answers.number);
-    return manager;
-})
-
-//TODO: Prompt engineer
-function addEngineer(){
+function promptEmployee(){
     inquirer.prompt([
         {
             type: 'input',
             name:'name',
-            message: 'Enter the engineer name:',
+            message: 'Enter employee name:',
         },
         {
             type: 'input',
@@ -72,177 +28,73 @@ function addEngineer(){
             name:'email',
             message: 'Enter the employee email:',
         },
-        {
-            type: 'input',
-            name:'github',
-            message: 'Enter GitHub username:',
-        },
+      
         {
             type: 'list',
             name: 'question',
-            choices: ['Add an Intern', 'Finish building my team'],
+            message: 'What is the roll of this employee?',
+            //TODO: add list of options
+            choices: ['Manager', 'Engineer', 'Intern']
         }
-    ]).then((data) => {
-        if(data.question == 'Add an Intern'){
-            addIntern();
+    ]).then((answers) =>{
+        if(answers.question == 'Manager'){
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name:'number',
+                    message: 'Enter the office number:',
+                },
+            ]).then((data) =>{
+            const newManager = new Manager(answers.name, answers.id, answers.email, data.number);
+            team.push(newManager);
+            addOrQuit();
+        })
+        }
+        if(answers.question == 'Engineer'){
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name:'github',
+                    message: 'Enter Github username:',
+                },
+            ]).then((data) =>{
+            const newEngineer = new Engineer(answers.name, answers.id, answers.email, data.github);
+            team.push(newEngineer);
+            addOrQuit();
+        })
+        }
+        if(answers.question == 'Intern'){
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name:'school',
+                    message: 'Enter Intern school:',
+                },
+            ]).then((data) =>{
+            const newIntern = new Intern(answers.name, answers.id, answers.email, data.school);
+            team.push(newIntern);
+            addOrQuit();
+        })
+        }
+    })
+}    
+
+function addOrQuit(){
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'question',
+            message: 'Do you want to add another employee?',
+            //TODO: add list of options
+            choices: ['Yes', 'Quit'],
+        }
+    ]).then((data) =>{
+        if(data.question == 'Yes'){
+            promptEmployee();
         }else{
-            console.log('Your HTML has been created')
+            fs.writeFile('index.html',generateTeam(team),err => err? console.log(err):'');
         }
-        const engineer = new Engineer(data.name, data.id, data.email, data.github);
-        return engineer;
-        
     })
 }
-//TODO prompt intern
-function addIntern(){
-    inquirer.prompt([
-        {
-            type: 'input',
-            name:'name',
-            message: 'Enter the intern name:',
-        },
-        {
-            type: 'input',
-            name:'id',
-            message: 'Enter the employee ID:',
-        },
-        {
-            type: 'input',
-            name:'email',
-            message: 'Enter the employee email:',
-        },
-        {
-            type: 'input',
-            name:'school',
-            message: 'Enter intern school:',
-        },
-        {
-            type: 'list',
-            name: 'question',
-            choices: ['Finish building my team'],
-        }
-    ]).then((data) => {
-        if(data.choices == 'Finish building my team'){
-            console.log('Your HTML has been created')
-        }
-        const intern = new Intern(data.name, data.id, data.email, data.school);
-        return intern;
-    })
-}
-// TODO: What is this team parameter that got passed in?
-const generateTeam = team => {
-    // create the manager html
-    const generateManager = manager => {
-        return `
-        <div class="card employee-card">
-        <div class="card-header">
-            <h2 class="card-title">${manager.getName()}</h2>
-            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
-        </div>
-        <div class="card-body">
-            <ul class="list-group">
-                <li class="list-group-item">ID: ${manager.getId()}</li>
-                <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
-                <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
-            </ul>
-        </div>
-    </div>
-        `;
-    };
 
-    // create the html for engineers
-    const generateEngineer = engineer => {
-        return `
-        <div class="card employee-card">
-    <div class="card-header">
-        <h2 class="card-title">${engineer.getName()}</h2>
-        <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
-    </div>
-    <div class="card-body">
-        <ul class="list-group">
-            <li class="list-group-item">ID: ${engineer.getId()}</li>
-            <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
-            <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a></li>
-        </ul>
-    </div>
-</div>
-        `;
-    };
 
-    // create the html for interns
-    const generateIntern = intern => {
-        return `
-        <div class="card employee-card">
-    <div class="card-header">
-        <h2 class="card-title">${intern.getName()}</h2>
-        <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
-    </div>
-    <div class="card-body">
-        <ul class="list-group">
-            <li class="list-group-item">ID: ${intern.getId()}</li>
-            <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
-            <li class="list-group-item">School: ${intern.getSchool()}</li>
-        </ul>
-    </div>
-</div>
-        `;
-    };
-
-    const html = [];
-
-    html.push(team
-        .filter(employee => employee.getRole() === "Manager")
-        .map(manager => generateManager(manager))
-    );
-    html.push(team
-        .filter(employee => employee.getRole() === "Engineer")
-        .map(engineer => generateEngineer(engineer))
-        .join("")
-    );
-    html.push(team
-        .filter(employee => employee.getRole() === "Intern")
-        .map(intern => generateIntern(intern))
-        .join("")
-    );
-
-    return html.join("");
-
-}
-
-// export function to generate entire page
-module.exports = team => {
-
-    return `
-    <!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>My Team</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
-    <script src="https://kit.fontawesome.com/c502137733.js"></script>
-</head>
-
-<body>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-12 jumbotron mb-3 team-heading">
-                <h1 class="text-center">My Team</h1>
-            </div>
-        </div>
-    </div>
-    <div class="container">
-        <div class="row">
-            <div class="team-area col-12 d-flex justify-content-center">
-                ${generateTeam(team)}
-            </div>
-        </div>
-    </div>
-</body>
-</html>
-    `;
-};
